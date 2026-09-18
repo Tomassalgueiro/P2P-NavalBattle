3b6f4ebe27349c9be5f5720568c1723f8f50faac
guinhas <tomasthor8@gmail.com>
2026-09-18 14:06:07 +0100

    Table logic done

--- src/table.py
+++ src/table.py
@@ -36,13 +36,17 @@ class Table:
             # inside bounds, verify for presence of another ship
             if x + length < self.size:  
                 for i in range(length):
-                    if self.grid[x+1][y] == 'S':
+                    if not self.is_valid_coordinate(x+i,y):
+                        return False
+                    if self.grid[x+1][y] != EMPTY:
                         return False
         else:
             # inside bounds, verify for presence of another ship
             if y + length < self.size: 
                 for i in range(length):
-                    if self.grid[x][y+1] == 'S':
+                    if not self.is_valid_coordinate(x,y+i):
+                        return False
+                    if self.grid[x][y+1] != EMPTY:
                         return False
         return True
 
@@ -54,8 +58,45 @@ class Table:
          
         if not self.can_place_ship(length,x,y,horizontal):
             return False
+
+        ship_coords: Set[Tuple[int,int]] = set()
+
+        for i in range(length):
+            cx = x + i if horizontal else x
+            cy = y  if horizontal else y + 1
+            self.grid[cx][cy] = SHIP
+            ship_coords.add((cx,cy))
+
+        self.ships[name] = ship_coords
         return True
 
+    def receive_shot(self, x: int, y: int) -> dict:
+        
+        if not self.is_valid_coordinate(x,y):
+            return { "valid": False, "status": "INVALID", "sunk": None, "game_over": False}
+
+        current = self.grid[x][y]
+
+        if current in (HIT,MISS):
+            return { "valid": False, "status": "ALREADY_SHOT", "sunk": None, "game_over": False}
+
+        if current == EMPTY :
+            return { "valid": True, "status": "MISS", "sunk": None, "game_over": False}
+
+        self.grid[x][y] = HIT
+        sunk_ship_name = None
+
+        for name,coord in self.ships.items():
+            if (x,y) in coord:
+                coord.remove((x,y))
+                if len(coord) == 0:
+                    sunk_ship_name = name
+
+        return { "valid": True, "status": "HIT", "sunk": sunk_ship_name, "game_over": self.is_game_over() }
+
+
+    def is_game_over(self) -> bool:
+       return all(len(coords) == 0 for coords in self.ships.values()) 
 
     #for each line on the grid, print it
     def display(self, hide_ships = False) -> None:
